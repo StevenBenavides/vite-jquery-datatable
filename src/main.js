05874d2669f82a595d6460c3a12902f75549c796
Stevenbenavidesj@gmail.com <147666314+StevenBenavides@users.noreply.github.com>
2026-05-03 15:07:18 -0500

    1

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import './style.css';
 import DataTable from 'datatables.net-dt';
 import 'datatables.net-dt/css/dataTables.dataTables.css';
 
-const API_URL = "https://raw.githubusercontent.com/PlataformasWeb-P-AA2026/api-demo/main/db.json"
+const API_URL = "https://gist.githubusercontent.com/AnndresRodriguez/a4216e3f82f45fc4514dc954f967fe9a/raw/models.json";
 
 async function cargarDatos() {
   try {
@@ -12,17 +12,24 @@ async function cargarDatos() {
       throw new Error('Error al consumir la API');
     }
 
-    const datos = await respuesta.json();
+    const json = await respuesta.json();
+    
+    const datos = json.marcas;
 
     new DataTable('#tabla-posts', {
       data: datos,
       columns: [
-        { data: 'AMIE' },
-        { data: 'Nombre-Educativa' },
-        { data: 'Canton' },
-        { data: 'Parroquia' }
+        { data: 'id', title: 'ID Marca' },
+        { data: 'nombre', title: 'Marca' },
+        { 
+          data: 'modelos', 
+          title: 'Modelos disponibles',
+          render: function (data) {
+            return data.join(', ');
+          }
+        },
       ],
-      pageLength: 10,
+      pageLength: 5,
       language: {
         search: 'Buscar:',
         lengthMenu: 'Mostrar _MENU_ registros',
